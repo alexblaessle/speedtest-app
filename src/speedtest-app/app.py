874c72a89abe4eb68dc731d3561164b580bc2088
Alexander Herrmann <alex@Alexanders-MacBook-Air.local>
2025-01-19 00:07:46 +0100

    moved to dash for app

--- src/speedtest-app/app.py
+++ src/speedtest-app/app.py
@@ -1,18 +1,61 @@
-import streamlit as st
 import pandas as pd
+import logging
+from dash import Dash, html, dcc, Input, Output, State, callback
+import plotly.express as px
 
-st.title("Speedtest-app")
+__data_csv__ = "speed_test_results.csv"
 
-def load_data(fn):
-    df=pd.read_csv(fn)
-    df=df.melt(id_vars=['timestamp'])
+logging.getLogger().setLevel(logging.INFO)
+
+
+def load_data(fn=None):
+
+    if not fn:
+        fn = __data_csv__
+
+    df = pd.read_csv(fn)
+    df = df.melt(id_vars=["timestamp"])
+
+    logging.info(f"Loaded {fn}.")
     return df
 
-df = load_data("speed_test_results.csv")
 
-st.subheader("Download/upload speeds")
-st.line_chart(data=df[~df['variable'].str.contains('ping')],x="timestamp",y="value",color='variable')
+def update_plot(df, variables):
 
-st.subheader("Ping")
-st.line_chart(data=df[df['variable'].str.contains('ping')],x="timestamp",y="value",color='variable')
+    fig = px.line(
+        df[df["variable"].str.contains(variables)],
+        x="timestamp",
+        y="value",
+        color="variable",
+    )
+    return fig
 
+
+app = Dash()
+
+app.layout = [
+    html.Button("Refresh", id="btn_refresh", n_clicks=0),
+    html.Div(children="Download/Upload speed"),
+    dcc.Graph(figure={}, id="plot_speed_line"),
+    html.Div(children="Ping"),
+    dcc.Graph(figure={}, id="plot_ping_line"),
+]
+
+
+@callback(
+    Output(component_id="plot_speed_line", component_property="figure"),
+    Output(component_id="plot_ping_line", component_property="figure"),
+    Input(component_id="btn_refresh", component_property="n_clicks"),
+)
+def update_graphs(n_clicks):
+    if n_clicks > -1:
+        print("Updating")
+        df = load_data()
+        fig_speed = update_plot(df, "(download)|(upload)")
+        fig_ping = update_plot(df, "ping")
+
+        return fig_speed, fig_ping
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/speedtest-app/run_speedtest.py
+++ src/speedtest-app/run_speedtest.py
@@ -10,12 +10,13 @@ from speed import SpeedtestLogger
 @click.option("-i", "--interval", default=1, help="Interval between tests.")
 @click.option("-o", "--out", default="speed_test_results.csv", help="Output file.")
 @click.option("-l", "--logging_level", default=20, help="Logging level.")
-def run(ntests,interval, out,logging_level):
+def run(ntests, interval, out, logging_level):
 
     # Set logging level
     logging.getLogger().setLevel(logging_level)
 
     SpeedtestLogger(out).log_tests(ntests)
+
 
 if __name__ == "__main__":
     run()
